d8cc49f110a0ec0a476ec4f7dc7f6f16f38adfdc
Arun Shivhare <arunshivhare925@gmail.com>
2026-03-23 15:44:18 +0530

    protect routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,16 +4,35 @@ import Dashboard from "../pages/Dashboard";
 import Visualizer from "../pages/Visualizer";
 import Explore from "../pages/Explore";
 import Login from "../pages/Login";
+import ProtectedRoute from "../components/ProtectedRoute";
 
-const AppRoutes = ({user}) => {
+const AppRoutes = ({ user }) => {
   return (
-      <Routes>
-        <Route path="/" element={<Home user={user} />} />
-        <Route path="/explore" element={<Explore user={user}/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/visualizer/:type" element={<Visualizer />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home user={user} />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute user={user}>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/visualizer/:type"
+        element={
+          <ProtectedRoute user={user}>
+            <Visualizer />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="/explore" element={<Explore user={user} />} />
+    </Routes>
   );
 };
 
